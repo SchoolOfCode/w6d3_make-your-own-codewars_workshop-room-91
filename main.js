8657e78ba1efb9a8dcda76a7f08c5b0d2b51866e
Ilie1985 <ilie.marian32@yahoo.com>
2021-12-22 16:51:07 +0000

    afternoon 2nd workshop task2

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 /* 
 👉 Write your kata here!
 */
+/*James has a math test and want to calculate the total score 
+mental math test , calculator test ,non calculator test
+complete the function to return the total number of all his math test*/
+
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export  function score(mentalMathScore,calculatorTestScore,nonCalculatorTestScore){
+    // work out your answer here
+    const result=  calculatorTestScore + nonCalculatorTestScore;
+    return result;
+}
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,28 @@
 //👉 Write your tests below here:
+
+import {score}  from './main.js'
+
+describe('Find the sum of James`s test', function () {
+    test('test the sum of James`s exams', function () {
+       
+        const mentalMathScore = 20;
+        const calculatorTestScore = 35;
+        const nonCalculatorTestScore = 50;
+        const actual = mentalMathScore + calculatorTestScore + nonCalculatorTestScore;
+        const expected = score(mentalMathScore,calculatorTestScore,nonCalculatorTestScore);
+       // jest.setTimeout(newTimeout)
+        expect(actual).toStrictEqual(expected)
+        
+           
+        console.log('this is the actual',actual);
+        console.log('this is expected', expected);
+    })
+})
+
+/*describe("Tests",function score ()  {
+    it("test", function score()  {
+  test.assertEquals(score(0,0,0), 0)
+  test.assertEquals(score(43, 10, 5), 58)
+    });
+  });*/
+  
