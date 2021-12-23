13387baecaa8a5545b4a660d04c1a8d3f965c76e
Ilie1985 <ilie.marian32@yahoo.com>
2021-12-23 14:09:24 +0000

    codewars workshop

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 /* 
 👉 Write your kata here!
 */
-/*James has a math test and want to calculate the total score 
-mental math test , calculator test ,non calculator test
-complete the function to return the total number of all his math test*/
+/*James has several math tests results and he wants to calculate the total .
+
+mental math test 
+calculator test 
+non calculator test
+
+Complete the function to return the total of all his math test results*/
 
 
 //👉 Write the function your CodeWarriors will start with below here:
 
 export  function score(mentalMathScore,calculatorTestScore,nonCalculatorTestScore){
     // work out your answer here
-    const result=  calculatorTestScore + nonCalculatorTestScore;
-    return result;
+    
 }
 
